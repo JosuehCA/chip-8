81e58c407f969763580497df7ad8c2c4d647e512
JosuehCA <josuemago2012@gmail.com>
2026-04-07 11:25:45 -0600

    Added ROM loading logic and empty constructor

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <cstdint>	// fixed-width types
+#include <fstream>	// file system manipulation
+
+const unsigned int PC_START_ADDRESS = 200;
 
 class Chip8 {
 public:
@@ -13,7 +16,29 @@ public:
 	uint8_t keypad[16]{};
 	uint32_t gfx[64 * 32]{};
 	uint16_t opcode;
+
+	Chip8();
+	void LoadROM(const char* filename);
 };
+
+void Chip8::LoadROM(const char* filename) {
+	std::ifstream file(filename, std::ios::binary | std::ios::ate);
+
+	if(file.is_open()) {
+		std::streampos size = file.tellg();
+		char* buffer = new char[size];
+
+		file.seekg(0, std::ios::beg);
+		file.read(buffer, size);
+		file.close();
+
+		for(long i = 0; i < size; i++) {
+			memory[PC_START_ADDRESS + i] = buffer[i];
+		}
+
+		delete[] buffer;
+	}
+}
 
 int main() {
 	
